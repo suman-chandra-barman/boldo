f104a619b2c584e7e7de2365833ca442a38db802
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2023-01-17 10:22:50 +0600

    boldo project done

--- src/pages/Home/CallToAction/CallToAction.jsx
+++ src/pages/Home/CallToAction/CallToAction.jsx
@@ -4,7 +4,7 @@ const CallToAction = () => {
   return (
     <section className="max-w-[1200px] bg-[#0A2640] py-10 lg:py-[72px] mx-auto text-white md:rounded-xl relative z-[1] overflow-hidden">
       <div className="md:w-[715px] mx-auto">
-        <h4 className="text-3xl md:text-[48px] text-center md:leading-[50px] lg:leading-[72px]">
+        <h4 className="text-3xl px-3 md:text-[48px] text-center md:leading-[50px] lg:leading-[72px]">
           An enterprise template to ramp up your company website
         </h4>
         <form className="mt-[50px] md:flex justify-center gap-6 text-center">
@@ -13,7 +13,7 @@ const CallToAction = () => {
             placeholder="Your email address"
             className="md:text-xl px-3 md:px-8 py-2 md:py-3 rounded-[256px] lg:w-[370px] "
           />
-          <button className="bg-[#69E6A6] font-bold mt-2 md:mt-0 md:text-xl text-[#0A2640] rounded-[56px] px-4 md:px-7 lg:px-[56px] py-2 md:py-4 hover:bg-white">
+          <button className="bg-[#69E6A6] font-bold mt-2 md:mt-0 md:text-xl text-[#0A2640] rounded-[56px] px-3 md:px-7 lg:px-[56px] py-2 md:py-4 hover:bg-white">
             Start now
           </button>
         </form>
--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -3,9 +3,9 @@ import TestimonialCarousel from "./TestimonialCarousel";
 
 const Testimonial = () => {
   return (
-    <section className="bg-[#0A2640]">
+    <section className="bg-[#0A2640] px-3">
       <div className="max-w-[1100px] mx-auto text-white pb-5 md:pb-10 lg:pb-[96px]">
-        <h3 className="px-2 md:px-0 text-2xl md:text-3xl lg:text-5xl md:leading-[72px] font-Manrope max-w-[716px] pt-[96px] mb-[72px]">
+        <h3 className="md:px-0 text-2xl md:text-3xl lg:text-5xl md:leading-[72px] font-Manrope max-w-[716px] pt-[96px] mb-[72px]">
           An enterprise template to ramp up your company website
         </h3>
         <TestimonialCarousel />
